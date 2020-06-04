11644892902bc69c3e76c8613efb4eaf1c6a9584
Gustavo Carvalho <gustavo22carvalho@gmail.com>
2020-06-03 22:42:44 -0300

    Commit inicial

---
+++ src/main/java/com/ibm/testclientmovies/TestClientMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.testclientmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClientMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestClientMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/testclientmovies/TestClientMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.testclientmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClientMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
